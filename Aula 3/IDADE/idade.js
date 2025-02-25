e26b20b94455240828fcf246264985d87db8b62d
Yunnakym <annapoliane2@gmail.com>
2025-02-24 21:12:20 -0300

    versão 2
    Modificação HTML, JS

--- Aula 3/IDADE/idade.js
+++ Aula 3/IDADE/idade.js
@@ -1,12 +1,19 @@
 
-function calcular(){
+function verificar(){
     let idade = Number(document.getElementById("idade").value);
+    let imgVerifica = document.getElementById("imgVerifica");
 
     if(idade >= 18){
-        resultado.innerHTML = "Idade maior que 18, acesso liberado! "
+      // resultado.innerHTML = "Idade maior que 18, acesso liberado! "
+       alert("Idade maior que 18, acesso liberado!")
+        
+        imgVerifica.src = "aprovado.png"
     }
   
     else{
-        resultado.innerHTML = "Aesso Negado!"
+        //resultado.innerHTML = "Acesso Negado!"
+        alert("Acesso Negado!")
+
+        imgVerifica.src = "negado.png"
     }
 }
